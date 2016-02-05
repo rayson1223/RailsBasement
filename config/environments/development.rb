df3786fb03a02e97343ccf57ec1a8ed241884ede
Rayson <raysonlcp1223@gmail.com>
2016-02-05 19:46:09 +0800

    + Controller: Added Home controller + index pages + Devise Install

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,8 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 
